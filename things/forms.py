0b3e7fc0bbb8c15f38603add7dae81a4f843e596
mmmmie <4mmmmie@gmail.com>
2023-10-16 13:43:55 +0100

    Add constraints to ThingForm fields.

--- things/forms.py
+++ things/forms.py
@@ -1,8 +1,9 @@
 """Forms of the project."""
 from django import forms
+from django.core import validators
 
 
 class ThingForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    description = forms.CharField(widget=forms.Textarea)
-    quantity = forms.IntegerField(widget=forms.NumberInput)
+    name = forms.CharField(max_length=36, required=True)
+    description = forms.CharField(widget=forms.Textarea, required=True)
+    quantity = forms.IntegerField(widget=forms.NumberInput, min_value=0, max_value=50)
